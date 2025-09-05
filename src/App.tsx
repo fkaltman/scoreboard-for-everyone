aaadc5ddab1751232b1fda9f65314e100815fea4
Francine Altman <francinekline@gmail.com>
2025-09-05 10:25:29 -0400

    Fix: react import breaking build step

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { TeamSection } from "./components/TeamSection";
 import { TeamSettings } from "./components/TeamSettings";
 import { useScoreboard } from "./hooks/useScoreboard";
